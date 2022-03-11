38e527e873a8a90f4a6ac2a277ca75966f3704b6
nukumalik <nukumalik@gmail.com>
2022-03-11 16:14:51 +0700

    feat: add logger

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser'
 import {Router} from 'express'
+import morgan from 'morgan'
 
 export const middlewares = Router()
   .use(bodyParser.urlencoded({extended: false}))
   .use(bodyParser.json())
+  .use(morgan('dev'))
