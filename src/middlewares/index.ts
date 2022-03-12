23ee1e19b784e2227e2d5d72e697f00cdb1b789f
nukumalik <nukumalik@gmail.com>
2022-03-12 13:57:49 +0700

    feat: add cors middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,9 +2,11 @@ import bodyParser from 'body-parser'
 import {Router} from 'express'
 import morgan from 'morgan'
 import passport from 'passport'
+import cors from 'cors'
 
 export const middlewares = Router()
   .use(bodyParser.urlencoded({extended: false}))
   .use(bodyParser.json())
   .use(morgan('dev'))
   .use(passport.initialize())
+  .use(cors())
